b68dfbaa7889a2cbab171a854f5bd419b9e0b48f
buaashuai <909104292@qq.com>
2016-10-08 13:27:49 +0800

    完善ignore

--- src/pers/sharedFileSystem/test/exam/Demo1.java
+++ src/pers/sharedFileSystem/test/exam/Demo1.java
@@ -6,4 +6,10 @@ package pers.sharedFileSystem.test.exam;
 public class Demo1 {
     String str1 = "str1";
     public String age = "23";
+    public int add(int a, int b){
+        return a+b;
+    }
+    public Demo1(){
+        System.out.println("demo1");
+    }
 }
--- src/pers/sharedFileSystem/test/exam/Demo2.java
+++ src/pers/sharedFileSystem/test/exam/Demo2.java
@@ -4,15 +4,32 @@ package pers.sharedFileSystem.test.exam;
  * Created by dell on 2016/8/30.
  */
 public class Demo2 extends Demo1{
+    public Demo2(){
+        System.out.println("demo2");
+    }
     public void m1(){
         System.out.println(this.age);
         System.out.println(this.str1);
     }
+    public void testThread(){
+        Thread thread =new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("pang");
+            }
+        });
+        thread.start();
+        System.out.println("ping");
+    }
+    public int add(int a){
+        return a+1;
+    }
     public static void main(String[] args) {
-        Demo2 demo2=new Demo2();
-        demo2.m1();
         Demo1 demo1=new Demo1();
-        System.out.println(demo1.age);
-        System.out.println(demo1.str1);
+        Demo2 demo2=new Demo2();
+        demo2.testThread();
+        System.out.println(demo2.add(1,2));
+        String s;
+        System.out.println("s=");
     }
 }
